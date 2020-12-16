82f885305954aba40eb0bc81131b74776e5685a0
Ryan Dunning <ryandunning57@gmail.com>
2020-12-15 18:39:58 -0800

    stuff removed

--- Peer.java
+++ Peer.java
@@ -53,6 +53,7 @@ class Peer extends Object {
 			System.out.println("messge sent");
 			out.close();
 		} catch (IOException e) {
+			System.err.println(this.id);
 			System.err.println("Message Not sent!");
 			e.printStackTrace();
 		}
